d79a84b39a74c215ca9fd2565cd7c7e0e7971faa
Mykola Tsypa <mtsypa@ego-cms.com>
2020-05-15 14:11:51 +0300

    removed sendgreid key

--- back/config/index.js
+++ back/config/index.js
@@ -8,7 +8,6 @@ let data = {
     PORT: 9000,
     MONGOURI: 'mongodb://localhost:27017/',
     DB_NAME: 'marchtach',
-    SENDGRID_API_KEY: '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' 
 }
 
 if (process.env.NODE_ENV === "prod") {
