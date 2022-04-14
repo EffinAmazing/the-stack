e5d133859965106026c76d89929410a2bfdb9ced
Joel Hatmaker <joel@mcgaw.io>
2022-04-14 11:34:32 -0700

    allow 403 to continue for domains

--- back/core/controllers/blueprints.js
+++ back/core/controllers/blueprints.js
@@ -46,14 +46,18 @@ class BluePrints {
                     //lets expose errors clientside so we can 
                     //get better feedback
                     let returnErrMessage = "Website Not Found";
-  
-                    if (err.response) {                        
+                    
+                    if (err && err.message == "statusCode=403") {
+                        cb(null);
+                    } else if (err.response) {                        
                         // The request was made and the server responded with a status code
                         // that falls out of the range of 2xx
                         console.log(err.response.data);
                         console.log(err.response.status);
                         console.log(err.response.headers);
                         returnErrMessage += ': '+err.response.status+' '+err.response.statusText;
+                        
+
                       } else if (err.request) {                        
                         // The request was made but no response was received
                         // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
