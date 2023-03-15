2a3ebce42b394a04f0bfa4c83e539907c9c95f32
Joel Hatmaker <joel@mcgaw.io>
2023-03-15 12:46:20 -0700

    fix email grammar

--- back/services/email.js
+++ back/services/email.js
@@ -10,17 +10,17 @@ module.exports.sendInviteForUser = async function (email, user, frontPath, bluep
     const linkBluePrint = frontPath + '#/stack/build/' + blueprint.id;
     const link = frontPath + '#/profile/verify/' + user.validationCode;
     
-    const textInviteMail = 'Hi,\n You have got invite to edit stack, \n please go via link to complete registration: <' + link + '> \n\n Thanks! \n McGaw.io Team';
+    const textInviteMail = 'Hi,\n You have been invited to edit a stack in the Marketing Technologies Stack Builder, \n please accept your invitation via this link to complete registration: <' + link + '> \n\n Thanks! \n McGaw.io Team';
     const htmlInviteMail = staticMail.mailStyle + staticMail.mailHeader(frontPath + '#/')  + 
-        '<p> &nbsp; </p><div ' + staticMail.mailPStyles + '> Hi, </div><div ' + staticMail.mailPStyles + '>You have got invite to edit stack,</div><div ' + staticMail.mailPStyles + '> please go via link to complete registration: </div> ' +
+        '<p> &nbsp; </p><div ' + staticMail.mailPStyles + '> Hi, </div><div ' + staticMail.mailPStyles + '>You have been invited to edit a stack in the Marketing Technologies Stack Builder,</div><div ' + staticMail.mailPStyles + '> please accept your invitation via this link to complete registration: </div> ' +
         '<p style="text-align: center;"> <img src="' + config.SERVER_URI + '/domain-logos/' + blueprint.domain + '.png" /> <br /> <b style="font-size: 18px;">' + blueprint.domain + '</b> </p>' +
         '<p style="text-align: center;"> <a  ' + staticMail.mailButtonStyles + ' href="' + link + '">  Visit the Stack  </a>  </p>' +
         staticMail.mailFooter;
 
-    const textConfirmationMail = 'Hi, ' + user.firstName + ' ' + user.lastName + ' \n You have been added to a new stack, \n you can view it by link: <' + linkBluePrint + '> \n\n Thanks! \n McGaw.io Team';
+    const textConfirmationMail = 'Hi ' + user.firstName + ' ' + user.lastName + ', \n You have been added to a new stack in the Marketing Technologies Stack Builder, \n you can view it via this link: <' + linkBluePrint + '> \n\n Thanks! \n McGaw.io Team';
     const htmlConfirmationMail = staticMail.mailStyle + staticMail.mailHeader(frontPath + '#/')  + 
-    '<p> &nbsp; </p><div ' + staticMail.mailPStyles + '> Hi, <b> ' + user.firstName + ' ' + user.lastName + ' </b> </div>' + 
-    '<div ' + staticMail.mailPStyles + '> <p ' + staticMail.mailPStyles + '> You have been added to a new stack, you can view it by link: </div> ' +
+    '<p> &nbsp; </p><div ' + staticMail.mailPStyles + '> Hi <b> ' + user.firstName + ' ' + user.lastName + ', </b> </div>' + 
+    '<div ' + staticMail.mailPStyles + '> <p ' + staticMail.mailPStyles + '> You have been added to a new stack in the Marketing Technologies Stack Builder, you can view it via this link: </div> ' +
     '<p style="text-align: center;"> <img src="' + config.SERVER_URI + '/domain-logos/' + blueprint.domain + '.png" /> <br /> <b style="font-size: 18px;">' + blueprint.domain + '</b> </p>' +
     '<p style="text-align: center;"> <a ' + staticMail.mailButtonStyles + ' href="' + linkBluePrint + '"> Visit the Stack </a> </p><p> &nbsp; </p>'
     +  staticMail.mailFooter;
