f45aca36ed2af2d5b957dd0fd7637ab143d47184
Joel Hatmaker <joel@mcgaw.io>
2025-10-03 15:05:01 -0700

    cleanup

--- back/services/sitetechonlogies.js
+++ back/services/sitetechonlogies.js
@@ -66,7 +66,7 @@ exports.getDomainTool = async function(domain) {
     const _path = path.resolve(__dirname, '../public/domain-logos', domain + '.png');
     let tool;
 
-    const token = LOGODEV_API_KEY; // Replace with your actual logo.dev token
+    const token = LOGODEV_API_KEY;
     const logoUrl = `https://img.logo.dev/${domain}?token=${token}`;
 
     try {
